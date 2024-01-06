b9922f77ed5b6bf088afbae6a402c6b9bd1a03a1
geduardo <3duardo.oc@gmail.com>
2024-01-06 10:31:02 -0600

    Agregando Docstrigns a list_creator.py

--- list_creator.py
+++ list_creator.py
@@ -1,3 +1,14 @@
 def get_list(dict, string):
+    """List generator from a dictionary
+
+    Args:
+        dict (dictionary): is the dictionary that you want to get the list from
+        string (string): is the key name that you want to get the list from 
+
+    Returns:
+        list: returns a list with the values from the key name 
+    """
     data = list(map(lambda item: item[string], dict))
     return data
+
+#help(get_list)
